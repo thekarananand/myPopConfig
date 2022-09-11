06ffed34b703609f4f0011f68230fab9a2635e6b
thekarananand <thekarananand@gmail.com>
2022-09-11 13:39:59 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,28 +10,20 @@ echo "--------------------------------------------------------------------------
 
 echo "Setting Up Microsoft's Repo for VS CODE"
 
-echo "[COMMAND] : wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg" >> logs
-echo "" >> logs
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg >> logs
+echo "[4 Commands for Microsoft's Repo]" >> logs
 
-echo "[COMMAND] : sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg" >> logs
-echo "" >> logs
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg >> logs
-
-echo "[COMMAND] : sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list' ">> logs
-echo "" >> logs
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-
-echo "[COMMAND] : rm -f packages.microsoft.gpg" >> logs
-echo "" >> logs
 rm -f packages.microsoft.gpg
+
 echo "---------------------------------------------------------------------------------------" >> logs
 
 echo "Setting Up Flathub"
 
-echo "[COMMAND] : flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo " >> logs
-echo "" >> logs
+echo "[COMMAND] : flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo "
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo >> logs
+
 echo "---------------------------------------------------------------------------------------" >> logs
 
 echo "Updating Flatpak"
@@ -43,18 +35,18 @@ echo "--------------------------------------------------------------------------
 
 echo "Removing Unnecessary .deb Packages"
 
-echo "[COMMAND] : " >> logs
+echo "[COMMAND] : sudo apt-get remove --purge libreoffice* gnome-weather gnome-calendar gnome-contacts totem geary seahorse -y " >> logs
 echo "" >> logs
-sudo nala remove --purge libreoffice* gnome-weather gnome-calendar gnome-contacts totem geary seahorse -y >> logs
+sudo apt-get remove --purge libreoffice* gnome-weather gnome-calendar gnome-contacts totem geary seahorse -y >> logs
 echo "---------------------------------------------------------------------------------------" >> logs
 
 echo "Cleaning Up"
 
-echo "[COMMAND] : nala clean" >> logs
+echo "[COMMAND] : sudo nala clean" >> logs
 echo "" >> logs
 sudo nala clean >> logs
 
-echo "[COMMAND] : nala autoremove " >> logs
+echo "[COMMAND] : sudo nala autoremove " >> logs
 echo "" >> logs
 sudo nala autoremove >> logs
 echo "---------------------------------------------------------------------------------------" >> logs
@@ -75,7 +67,7 @@ sudo nala update && sudo nala upgrade -y >> logs
 echo "---------------------------------------------------------------------------------------" >> logs
 
 echo "Installing Apps"
-echo "[COMMAND] : sudo nala install neofetch htop ubuntu-restricted-extras code telegrm-desktop discord gnome-software qbittorrent inkscape vlc  -y >> logs" >> logs
+echo "[COMMAND] : sudo nala install neofetch htop ubuntu-restricted-extras code telegram-desktop discord gnome-software qbittorrent inkscape vlc -y" >> logs
 echo "" >> logs
 sudo nala install neofetch htop ubuntu-restricted-extras code telegrm-desktop discord gnome-software qbittorrent inkscape vlc  -y >> logs
 echo "---------------------------------------------------------------------------------------" >> logs
