c6267544ecd579d753fc997d7c5543f44b3f64a0
thekarananand <thekarananand@gmail.com>
2022-09-13 11:23:32 +0530

    Add files via upload

---
+++ grub.sh
@@ -0,0 +1,17 @@
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
+sudo apt install grub-customizer grub-efi grub2-common -y
+sudo cp /boot/grub/x86_64-efi/grub.efi /boot/efi/EFI/pop/grubx64.efi
+
+mkdir -p "/boot/grub/themes/ROG"
+mkdir -p "/boot/grub/themes/Decent"
+
+cp -a ./themes/ROG/* "/boot/grub/themes/ROG"
+cp -a ./themes/Decent/* "/boot/grub/themes/Decent"
+
+echo 'GRUB_TIMEOUT_STYLE="menu"' >> /etc/default/grub
+echo 'GRUB_TIMEOUT="10"' >> /etc/default/grub
+# echo "GRUB_THEME=\"/boot/grub/themes/ROG/theme.txt\"" >> /etc/default/grub
+echo "GRUB_THEME=\"/boot/grub/themes/Decent/theme.txt\"" >> /etc/default/grub
+echo 'GRUB_GFXMODE="auto"' >> /etc/default/grub 
+
+sudo grub-customizer
