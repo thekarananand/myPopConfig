1f075546fd575032f999350267d0d4bb9834a96b
Karan Anand <thekarananand@gmail.com>
2022-09-18 08:32:32 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,25 @@
+cd ./scripts
+
+chmod +x ./pre_installation.sh
+./pre_installation.sh
+read -p "Press [ENTER] to Continue..."
+clear
+
+chmod +x ./installation.sh
+./installation.sh
+read -p "Press [ENTER] to Continue..."
+clear
+
+chmod +x ./SysConfig.sh
+./SysConfig.sh
+read -p "Press [ENTER] to Continue..."
+clear
+
+chmod +x ./extension.sh
+./extension.sh
+read -p "Press [ENTER] to Continue..."
+clear
+
+
+echo "Now,It's Ready!!..."
+neofetch
