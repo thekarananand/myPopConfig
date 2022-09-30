8256463ff9058c7e586228a703e90d5deca27189
Karan Anand <thekarananand@gmail.com>
2022-09-30 21:24:49 +0530

    Delete scripts directory

--- scripts/grubConfig.sh
+++
@@ -1,4 +0,0 @@
-echo 'GRUB_TIMEOUT_STYLE="menu"' >> /etc/default/grub
-echo 'GRUB_TIMEOUT="10"' >> /etc/default/grub
-echo "GRUB_THEME=\"/boot/grub/themes/Decent/theme.txt\"" >> /etc/default/grub
-echo 'GRUB_GFXMODE="auto"' >> /etc/default/grub 	
