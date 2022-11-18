3bc37f56e27af43b75bd47c788f9d814022ff4d6
Karan Anand <thekarananand@gmail.com>
2022-11-18 09:53:38 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,6 +37,10 @@ then
     
     # Install Pre-Req
     	
+		# Repo for nala
+		echo "deb [arch=amd64,arm64,armhf] http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
+		wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
+		
 		sudo apt update
 		sudo apt install nala git curl -y
 
